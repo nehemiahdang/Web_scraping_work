0d9129e41ab5431d0e29201f8d91feb5075509db
Nehemiah B. Dang <nehemiahdang@gmail.com>
2020-05-30 23:44:37 -0700

    Added Web Scraping challenge

---
+++ scrape_mars.py
@@ -0,0 +1,9 @@
+# Import Dependencies
+from bs4 import BeautifulSoup as bs
+import requests
+from splinter import Browser
+import os
+from pprint import pprint
+import cssutils
+import pandas as pd
+import time
