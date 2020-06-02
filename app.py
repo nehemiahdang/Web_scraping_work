6e4d8a0f105f49328bb20f927ad1af6645c2adf6
Nehemiah B. Dang <nehemiahdang@gmail.com>
2020-06-02 02:02:15 -0700

    Updated web_scraping challenged

--- app.py
+++ app.py
@@ -15,13 +15,33 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 @app.route("/")
 def home():
     news_data = mongo.db.news_data.find_one()
-    return render_template("index.html", text="Mission to Mars", news_data=news_data)
+    images_data = mongo.db.images_data.find_one()
+    weather_data = mongo.db.weather_data.find_one()
+    facts_data = mongo.db.facts_data.find_one()
+    return render_template("index.html", text="Mission to Mars", news_data=news_data, images_data=images_data, weather_data=weather_data, facts_data=facts_data)
 
 @app.route("/scrape")
 def scrape():
+    # Scrape for mars news data
     news_data = mongo.db.news_data
-    news_scrape_data = scrape_mars.scrape()
+    news_scrape_data = scrape_mars.scrape_news()
     news_data.update({}, news_scrape_data, upsert=True)
+
+    # Scrape for mars images data
+    images_data = mongo.db.images_data
+    images_scrape_data = scrape_mars.scrape_images()
+    images_data.update({}, images_scrape_data, upsert=True)
+
+    # Scrape for mars weather data
+    weather_data = mongo.db.weather_data
+    weather_scrape_data = scrape_mars.scrape_weather()
+    weather_data.update({}, weather_scrape_data, upsert=True)
+
+    # Scrape for mars facts data
+    facts_data = mongo.db.facts_data
+    facts_scrape_data = scrape_mars.scrape_facts()
+    facts_data.update({}, facts_scrape_data, upsert=True)
+
     return redirect("/", code=302)
 
 
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,6 +1,5 @@
 # Import Dependencies
 from bs4 import BeautifulSoup as bs
-import requests
 from splinter import Browser
 import os
 import cssutils
@@ -19,43 +18,101 @@ def init_browser():
 
 def scrape_news():
     # Save the init_browser() function as a variable
-    # browser = init_browser()
+    browser = init_browser()
 
     news_data = {}
     
     # Request from the url and store
     news_url = 'https://mars.nasa.gov/news/'
-    response = requests.get(news_url)
+
+    browser.visit(news_url)
+    time.sleep(5)
 
     # Scrape page into Soup
-    news_soup = bs(response.text, 'html.parser')
-
-    # Get latest news title
-    #news_title = news_soup.title.text
-
-    # Get latest news paragraph
-    #news_p = news_soup.p.text
+    news_html = browser.html
+    news_soup = bs(news_html, 'html.parser')
 
     # Store data to return
     news_data["title"] = news_soup.title.text
     news_data["paragraph"] = news_soup.p.text
+
+    browser.quit()
 
     return news_data
 
 
 
 
-# def scrape_images():
-#     browser = init_browser()
+def scrape_images():
+    browser = init_browser()
+
+    images_data = {}
+
+    mars_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+
+    browser.visit(mars_url)
+    time.sleep(5)
+
+    images_html = browser.html
+    images_soup = bs(images_html, 'html.parser')
+
+    art_style = images_soup.find('article')['style']
+    style = cssutils.parseStyle(art_style)
+    img_url = style['background-image']
+    img_url = img_url.replace('url(', '').replace(')', '')
+    featured_image_url = 'https://www.jpl.nasa.gov' + img_url
+    featured_image_url
+
+    images_data['image_url'] = featured_image_url
+
+    browser.quit()
+
+    return images_data
 
 
-# def scrape_weather():
-#     browser = init_browser()
+def scrape_weather():
+    browser = init_browser()
 
+    weather_data = {}
 
-# def scrape_facts():
-#     browser = init_browser()
+    twitter_url = 'https://twitter.com/marswxreport?lang=en'
 
+    browser.visit(twitter_url)
+    time.sleep(5)
+
+    twitter_html = browser.html
+    twitter_soup = bs(twitter_html, 'html.parser')
+
+    mars_tweets = twitter_soup.find_all('span')
+    mars_weather = []
+    for tweet in mars_tweets:
+        if 'InSight sol' in tweet.text:
+            mars_weather.append(tweet.text)
+            #print(mars_weather)
+    
+    weather_data['latest_tweet'] = mars_weather[0]
+
+    browser.quit()
+
+    return weather_data
+
+def scrape_facts():
+    browser = init_browser()
+    
+    #facts_data = {}
+
+    facts_url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(facts_url)
+
+    facts_df = tables[0]
+    facts_df.columns = ["Description", "Value"]
+    facts_df.set_index('Description', inplace=True)
+
+    facts_data = facts_df.to_dict()['Value']
+
+    browser.quit()
+
+    return facts_data
 
 # def scrape_hemi():
 #     browser = init_browser()
